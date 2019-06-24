3023c0febb9c2181e4e153a4ac3fb4504c87b034
Olle Thunberg <olle.thunberg03@gmail.com>
2019-06-24 21:34:34 +0200

    v1.0.0 of the wesbite :white_check_mark:

---
+++ build/precache-manifest.2751aff266264dafdb348fcc9a91d6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9626dc026cc4c4ae90f70429fa630327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0c40b1b72a05cdf8a48",
+    "url": "/static/css/main.9c01a576.chunk.css"
+  },
+  {
+    "revision": "e2fa2efd198a80753502",
+    "url": "/static/js/2.b2b59a1d.chunk.js"
+  },
+  {
+    "revision": "a0c40b1b72a05cdf8a48",
+    "url": "/static/js/main.6e0afca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
